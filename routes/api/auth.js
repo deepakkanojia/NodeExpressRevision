33d641c57e6b45afed43288840e7598f3e27eb3a
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-19 02:15:54 +0530

    added profile model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 const User = require('../../models/Users');
 const {check , validationResult } = require('express-validator');
 const config = require('config');
+const jwt = require('jsonwebtoken');
 
 // defining routes
 //test rout
@@ -20,19 +22,17 @@ router.get('/',auth , async(req,res)=>{
     console.error('error occured', err);
    }
 })
-
+//copied from users.js of api
 // @route POST api/auth
 // @description Authenticate user & get token
 // @access Public
+//it is a login page so we dont need to send the name
 
 router.post('/',[
-    check('name' , 'Name is required')
-    .not()
-    .isEmpty(),
     check('email' , 'Email is required')
     .isEmail(),
-    check('password' , 'enter password should be greater than 8 words')
-    .isLength({min : 8})
+    check('password' , 'password is required')
+    .exists()
 ] ,async(req,res)=>{
     // console.log(req.body);
     //ab sare result iske ander store honge
@@ -43,40 +43,26 @@ router.post('/',[
         return res.status(400).json({errors : errors.array()});
     }
 
-    const {name , email ,password } = req.body ;
+    const { email ,password } = req.body ;
     try{
     // see if the user exist
     //abhi yaha user naam ke variable me hum ek function perform ker rahe hai jisse email ko dunda jaye
         let user = await Users.findOne({email});
-        if(user){
-            return res.status(400).json([{msg :"user already present"}]);
+        if(!user){
+            return res
+            .status(400)
+            .json([{msg :"invalid credentials"}]);
         }
-
-        //creating instance of user
-            user = new Users({
-                name,
-                email,
-                gravatar,
-                password
-            });
-
-
-    //Get user gravatar
-    const avatar = gravatar.url(email , {
-        s : 200,
-        // s is for size
-        r : 'pg',
-        // r is for rating 
-        d : 'mm'
-        //d default icon 
-    })
-
-    //Encrypt password using bcrypt
-    // abhi yaha per salt variable lenge hashing ke liye
-    const salt = await bcrypt.genSalt(10);
-    // abhi yaha per hash kara aur hash me do arguments pass kara 
-    user.password = await bcrypt.hash(password,salt);
-
+        //to abhi yaha per isMatch me hum apne bcrypt ko call ker rahe hai aur vo compare ker raha hai simple string vs hashed string
+        const isMatch = await bcrypt.compare(password,user.password);
+        //ager nahi mila to ye hoga
+        if(!isMatch)
+        {
+            return res
+            .status(400)
+            .json([{msg :"invalid credentials"}]);
+        }
+   
     //return json Web token
 
         const payload = {
