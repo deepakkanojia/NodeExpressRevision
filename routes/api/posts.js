841720fbedeb6feae69c96dbd139bea5a2b754fe
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-20 13:11:48 +0530

    post model added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const {check , validationResult} = require('express-validator');
+const Post = require('../../models/Post');
 
-// defining routes
-//test rout
-// /api/posts
-router.get('/', (req,res)=>{
-    res.send('posts routes');
+// @private
+//@create a post
+// @post  /api/posts
+router.post('/',[auth,[
+    check('text' , 'text is required')
+    .not()
+    .isEmpty()
+]] , async(req,res)=>{
+    try {
+       const errors = validationResult(req);
+       if(!errors.isEmpty())
+       {
+           return res.status(400).json({errors : errors.array()});
+       } 
+
+
+    } catch (error) {
+        console.error('error' , error);
+    }
+    // res.send('posts routes');
 })
 
 module.exports = router;
